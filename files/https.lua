e90254060757df61be7b7e7c7e22c2dfa8fb37cd
MyNameIsTrez <welfje@gmail.com>
2021-02-23 00:07:29 +0100

    Working on /https-post

--- files/https.lua
+++ files/https.lua
@@ -10,6 +10,9 @@ end
 function post(query_url, post_data)
   local redirect_url = "http://h2896147.stratoserver.net:1337"
   local full_url = redirect_url .. "/https-post?url=" .. query_url
+  print("foo")
+  print("data={\"rofl\":\"" .. post_data .. "\"}")
+  print("bar")
   local response_table = http.post(full_url, "data={\"rofl\":\"" .. post_data .. "\"}")
   return response_table
 end
