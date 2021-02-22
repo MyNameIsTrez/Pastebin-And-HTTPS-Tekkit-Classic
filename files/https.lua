52c56b89ebef00fcd7156d868fc0827578a9204a
MyNameIsTrez <welfje@gmail.com>
2021-02-22 21:07:55 +0100

    Changed port from 3000 to 1337.

--- files/https.lua
+++ files/https.lua
@@ -1,7 +1,7 @@
 -- ComputerCraft and server code written and hosted by MyNameIsTrez#1585 on Discord. Feel free to contact me!
 
 function get(query_url)
-  redirect_url = "http://h2896147.stratoserver.net:3000"
+  redirect_url = "http://h2896147.stratoserver.net:1337"
   full_url = redirect_url .. "/https?url=" .. query_url
   response_table = http.get(full_url)
   if response_table == nil then error("Didn't get a response back; the HTTPS redirection server and/or the website at the URL passed to this function may be down.") end
